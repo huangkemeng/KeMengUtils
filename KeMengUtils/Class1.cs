18218384a531fc04df6e61ee2c40f5b98aef561e
huangkemeng <huangkemeng@outlook.com>
2019-06-19 14:09:51 +0800

    添加docfx文档生成

--- KeMengUtils/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace KeMengUtils
-{
-    public class Class1
-    {
-    }
-}
---
+++ KeMengUtils/RegexHelper/CommonRegexs.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+namespace KeMengUtils.RegexHelper
+{
+    public static class CommonRegexs
+    {
+        /// <summary>
+        /// 全部都是数字
+        /// </summary>
+        /// <returns></returns>
+        public static Regex OnlyNumber()
+        {
+            return new Regex(@"^[0-9]*$");
+        }
+
+        /// <summary>
+        /// 全部都是数字，且字符个数为m
+        /// </summary>
+        /// <param name="m">字符个数</param>
+        /// <returns></returns>
+        public static Regex OnlyMNumber(int m)
+        {
+            return new Regex(@"^\d{" + m + "}$");
+        }
+
+        /// <summary>
+        /// 全部都是数字，且字符个数至少为m
+        /// </summary>
+        /// <param name="m">字符个数</param>
+        /// <returns></returns>
+        public static Regex AtLeastMNumber(int m)
+        {
+            return new Regex(@"^\d{" + m + ",}$");
+        }
+
+        /// <summary>
+        /// 全部都是数字，且字符个数为m-n
+        /// </summary>
+        /// <param name="m">最少的字符个数</param>
+        /// <param name="n">最多的字符个数</param>
+        /// <returns></returns>
+        public static Regex OnlyNumberBetweenMN(int m, int n)
+        {
+            return new Regex(@"^\d{" + m + "," + "n" + "}$");
+        }
+    }
+}
