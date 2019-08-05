bc6fd66c30b71daae5ce969c4c20dcabe8e7d7c4
huangkemeng <huangkemeng@outlook.com>
2019-08-05 17:40:24 +0800

    优化了对DataTable的操作

--- KeMengUtils/DataHelper/DTHelper.cs
+++ KeMengUtils/DataHelper/DTHelper.cs
@@ -12,13 +12,9 @@ namespace KeMengUtils.DataHelper
     /// </summary>
     public static class DTHelper
     {
-        /// <summary>
-        /// 数据库连接字符串
-        /// </summary>
-
         private static string tempDbName = string.Empty;
 
-        private static Dictionary<string, string> _MappingDic = null;
+        private static Dictionary<string, string> _MappedDic = null;
 
         private static IEnumerable<string> _MappingSourceColumns = null;
 
@@ -38,13 +34,13 @@ namespace KeMengUtils.DataHelper
         }
 
         /// <summary>
-        /// 用于创建临时表时，生成一个由原表映射得到的列名的临时表
+        /// 用于创建临时表时的，生成一个由原表映射得到的列名的临时表
         /// </summary>
         /// <param name="dt"></param>
         /// <returns></returns>
         public static string CreateSqlColumnsString(this DataTable dt)
         {
-            return string.Join(",", dt.ColumnsAsEnumerable().Where(a => { var name = dt.TargetColumn(a.ColumnName); return !name.IsNullOrWhite(); }).Select(a => { var name = dt.TargetColumn(a.ColumnName); return $"[{name}] {a.GetSqlDataType()}"; }));
+            return string.Join(",", dt.ColumnsAsEnumerable().Where(a => { var name = dt.GetTargetColumn(a.ColumnName); return !name.IsNullOrWhite(); }).Select(a => { var name = dt.GetTargetColumn(a.ColumnName); return $"[{name}] {a.GetSqlDataType()}"; }));
         }
 
         /// <summary>
@@ -57,56 +53,7 @@ namespace KeMengUtils.DataHelper
             return string.Join(",", dt.ColumnsAsEnumerable().Select(a => $"[{a.ColumnName}] {a.GetSqlDataType()}"));
         }
 
-        /// <summary>
-        /// 转成可空的Int类型
-        /// </summary>
-        /// <param name="obj"></param>
-        /// <returns></returns>
-        public static int? toInt32(this object obj)
-        {
-            try
-            {
-                return Convert.ToInt32(obj);
-            }
-            catch (Exception)
-            {
-                return null;
-            }
-        }
 
-        /// <summary>
-        /// 转成可空的Decimal类型
-        /// </summary>
-        /// <param name="obj"></param>
-        /// <returns></returns>
-        public static decimal? toDecimal(this object obj)
-        {
-            try
-            {
-                return Convert.ToDecimal(obj);
-            }
-            catch (Exception)
-            {
-                return null;
-            }
-        }
-
-        /// <summary>
-        /// 转成可空的DateTime类型
-        /// </summary>
-        /// <param name="obj"></param>
-        /// <returns></returns>
-        public static DateTime? toDateTime(this object obj)
-        {
-            try
-            {
-                return Convert.ToDateTime(obj);
-            }
-            catch (Exception)
-            {
-                return null;
-            }
-        }
 
         /// <summary>
         /// 判断表格是否包含某些列名，返回不包含的列名
@@ -114,26 +61,21 @@ namespace KeMengUtils.DataHelper
         /// <param name="dt"></param>
         /// <param name="cols"></param>
         /// <returns></returns>
-        public static IEnumerable<string> NotContainColumns(this DataTable dt, IEnumerable<string> cols = null)
+        public static IEnumerable<string> NotContainColumns(this DataTable dt, params string[] cols)
         {
-            if (cols != null)
+            if (dt == null)
             {
-                foreach (string col in cols)
-                {
-                    if (!dt.Columns.Contains(col))
-                    {
-                        yield return col;
-                    }
-                }
+                throw new ArgumentNullException(nameof(dt));
             }
-            else
+            if (cols == null)
             {
-                foreach (string col in dt.MappingSourceColumns())
+                throw new ArgumentNullException(nameof(cols));
+            }
+            foreach (string col in cols)
+            {
+                if (!dt.Columns.Contains(col))
                 {
-                    if (!dt.Columns.Contains(col))
-                    {
-                        yield return col;
-                    }
+                    yield return col;
                 }
             }
         }
@@ -156,7 +98,7 @@ namespace KeMengUtils.DataHelper
         /// </summary>
         /// <param name="dt"></param>
         /// <param name="col_names">列名集合</param>
-        public static void CreateColumns(this DataTable dt, string[] col_names)
+        public static void CreateColumns(this DataTable dt, params string[] col_names)
         {
             foreach (string col_name in col_names)
             {
@@ -316,22 +258,26 @@ namespace KeMengUtils.DataHelper
         /// </summary>
         /// <param name="dt">表</param>
         /// <param name="dic">键值对</param>
-        /// <param name="clone_dr">复制的行</param>
-        public static void AddNewRow(this DataTable dt, Dictionary<string, object> dic = null, DataRow clone_dr = null)
+        /// <param name="clone_dr">复制的行,不复制则填null</param>
+        public static void AddNewRow(this DataTable dt, DataRow clone_dr = null, params KeyValuePair<string, object>[] dic)
         {
-            if (dic.Count > 0 && dic != null)
+            if (dt == null)
             {
-                DataRow dr = dt.NewRow();
-                if (clone_dr != null)
-                {
-                    dr.ItemArray = (object[])clone_dr.ItemArray.Clone();
-                }
+                throw new ArgumentNullException(nameof(dt));
+            }
+            DataRow dr = dt.NewRow();
+            if (clone_dr != null)
+            {
+                dr.ItemArray = (object[])clone_dr.ItemArray.Clone();
+            }
+            if (dic != null && dic.Length > 0)
+            {
                 foreach (KeyValuePair<string, object> row in dic)
                 {
                     dr[row.Key] = row.Value;
                 }
-                dt.Rows.Add(dr);
             }
+            dt.Rows.Add(dr);
         }
 
         /// <summary>
@@ -339,8 +285,16 @@ namespace KeMengUtils.DataHelper
         /// </summary>
         /// <param name="dt"></param>
         /// <param name="dics">数据字典</param>
-        public static void AddManyRows(this DataTable dt, IEnumerable<Dictionary<string, object>> dics)
+        public static void AddManyRows(this DataTable dt, params Dictionary<string, object>[] dics)
         {
+            if (dt == null)
+            {
+                throw new ArgumentNullException(nameof(dt));
+            }
+            if (dics == null)
+            {
+                throw new ArgumentNullException(nameof(dics));
+            }
             foreach (Dictionary<string, object> dic in dics)
             {
                 DataRow dr = dt.NewRow();
@@ -357,8 +311,16 @@ namespace KeMengUtils.DataHelper
         /// </summary>
         /// <param name="dt"></param>
         /// <param name="rows">DataRow</param>
-        public static void AddManyRows(this DataTable dt, IEnumerable<DataRow> rows)
+        public static void AddManyRows(this DataTable dt, params DataRow[] rows)
         {
+            if (dt == null)
+            {
+                throw new ArgumentNullException(nameof(dt));
+            }
+            if (rows == null)
+            {
+                throw new ArgumentNullException(nameof(rows));
+            }
             foreach (DataRow row in rows)
             {
                 dt.Rows.Add(row);
@@ -371,8 +333,17 @@ namespace KeMengUtils.DataHelper
         /// <param name="dt"></param>
         /// <param name="dic"></param>
         /// <returns></returns>
-        public static DataRow CreateNewRow(this DataTable dt, Dictionary<string, object> dic)
+        public static DataRow CreateNewRow(this DataTable dt, params KeyValuePair<string, object>[] dic)
         {
+            if (dt == null)
+            {
+                throw new ArgumentNullException(nameof(dt));
+            }
+
+            if (dic == null)
+            {
+                throw new ArgumentNullException(nameof(dic));
+            }
             DataRow dr = dt.NewRow();
             foreach (KeyValuePair<string, object> row in dic)
             {
@@ -387,8 +358,18 @@ namespace KeMengUtils.DataHelper
         /// <param name="dt">表</param>
         /// <param name="dics">行</param>
         /// <returns></returns>
-        public static IEnumerable<DataRow> CreateManyRows(this DataTable dt, IEnumerable<Dictionary<string, object>> dics)
+        public static IEnumerable<DataRow> CreateManyRows(this DataTable dt, params Dictionary<string, object>[] dics)
         {
+            if (dt == null)
+            {
+                throw new ArgumentNullException(nameof(dt));
+            }
+
+            if (dics == null)
+            {
+                throw new ArgumentNullException(nameof(dics));
+            }
+
             foreach (Dictionary<string, object> dic in dics)
             {
                 DataRow dr = dt.NewRow();
@@ -423,7 +404,7 @@ namespace KeMengUtils.DataHelper
         /// <param name="dic"></param>
         public static void SetMapping(this DataTable dt, Dictionary<string, string> dic)
         {
-            _MappingDic = dic;
+            _MappedDic = dic;
             _MappingSourceColumns = dic.Keys;
             _MappingTargetColumns = dic.Values;
         }
@@ -435,7 +416,7 @@ namespace KeMengUtils.DataHelper
         /// <returns></returns>
         public static Dictionary<string, string> MappingDic(this DataTable dt)
         {
-            return _MappingDic;
+            return _MappedDic;
         }
 
         /// <summary>
@@ -443,7 +424,7 @@ namespace KeMengUtils.DataHelper
         /// </summary>
         /// <param name="dt"></param>
         /// <returns></returns>
-        public static IEnumerable<string> MappingSourceColumns(this DataTable dt)
+        public static IEnumerable<string> GetMappingSourceColumns(this DataTable dt)
         {
             return _MappingSourceColumns;
         }
@@ -453,27 +434,36 @@ namespace KeMengUtils.DataHelper
         /// </summary>
         /// <param name="dt"></param>
         /// <returns></returns>
-        public static IEnumerable<string> MappingTargetColumns(this DataTable dt)
+        public static IEnumerable<string> GetMappingTargetColumns(this DataTable dt)
         {
             return _MappingTargetColumns;
         }
 
         /// <summary>
-        /// 通过数据库字段获取表字段
+        /// 通过数据库字段获取表字段名字
         /// </summary>
         /// <param name="dt"></param>
-        /// <param name="value">数据库字段</param>
+        /// <param name="field_name">数据库字段</param>
         /// <returns></returns>
-        public static string SourceColumn(this DataTable dt, string value)
+        public static string GetSourceColumnName(this DataTable dt, string field_name)
         {
-            try
+            if (_MappedDic == null)
             {
-                return _MappingDic.Where(a => a.Value == value).FirstOrDefault().Key;
+                throw new InvalidOperationException("调用此方法之前请先调用SetMapping方法设置表头到数据库字段的映射");
             }
-            catch (Exception)
+            if (dt == null)
             {
-                return null;
+                throw new ArgumentNullException(nameof(dt));
             }
+            if (string.IsNullOrWhiteSpace(field_name))
+            {
+                throw new ArgumentException("数据库表字段名不能为空或null", nameof(field_name));
+            }
+            if (!_MappedDic.Any(a => a.Value == field_name))
+            {
+                throw new KeyNotFoundException($"字典中未找到value为{field_name}的key");
+            }
+            return _MappedDic.Where(a => a.Value == field_name).FirstOrDefault().Key;
         }
 
         /// <summary>
@@ -482,11 +472,21 @@ namespace KeMengUtils.DataHelper
         /// <param name="dt">表</param>
         /// <param name="key">表字段</param>
         /// <returns></returns>
-        public static string TargetColumn(this DataTable dt, string key)
+        public static string GetTargetColumn(this DataTable dt, string key)
         {
-            string value = null;
-            _MappingDic.TryGetValue(key, out value);
-            return value;
+            if (_MappedDic == null)
+            {
+                throw new InvalidOperationException("调用此方法之前请先调用SetMapping方法设置表头到数据库字段的映射");
+            }
+            if (dt == null)
+            {
+                throw new ArgumentNullException(nameof(dt));
+            }
+            if (_MappedDic.TryGetValue(key, out string value))
+            {
+                return value;
+            }
+            throw new EvaluateException($"无法在设置的映射字典中通过表头名找到对应的数据库名");
         }
 
         /// <summary>
@@ -495,16 +495,39 @@ namespace KeMengUtils.DataHelper
         /// <param name="dr">行</param>
         /// <param name="value">值</param>
         /// <returns></returns>
-        public static object MappingValue(this DataRow dr, string value)
+        public static object GetMappedValue(this DataRow dr, string value)
         {
             try
             {
-                string key = _MappingDic.Where(a => a.Value == value).FirstOrDefault().Key;
-                return dr[key];
+                if (_MappedDic.Any(a => a.Value == value))
+                {
+                    string key = _MappedDic.Where(a => a.Value == value).FirstOrDefault().Key;
+                    return dr[key];
+                }
+                throw new KeyNotFoundException($"Excel表头映射数据库字段的字典中不包含名为{value}的数据库字段");
             }
             catch (Exception)
             {
                 return null;
+            }
+        }
+
+        public static bool TryGetValue(this DataRow dr, string key, out object value)
+        {
+
+            try
+            {
+                value = dr[key];
+                if (value == null)
+                {
+                    return false;
+                }
+                return true;
+            }
+            catch
+            {
+                value = null;
+                return false;
             }
         }
 
@@ -514,18 +537,17 @@ namespace KeMengUtils.DataHelper
         /// <param name="dr">行</param>
         /// <param name="key">值</param>
         /// <returns></returns>
-        public static object Value(this DataRow dr, string key)
+        public static object GetValue(this DataRow dr, string key)
         {
             try
             {
                 return dr[key];
             }
-            catch (Exception)
+            catch (Exception ex)
             {
-                return null;
+                throw new EvaluateException($"行中并不包含名为{key}的列", ex.GetInnermostException());
             }
         }
-
 
         /// <summary>
         /// 通过数据库字段名，设置原表的值
@@ -533,18 +555,23 @@ namespace KeMengUtils.DataHelper
         /// <param name="dr"></param>
         /// <param name="db_field"></param>
         /// <param name="value"></param>
-        public static void SetMappingValue(this DataRow dr, string db_field, object value)
+        public static void SetValueByMappedColumns<T>(this DataRow dr, string db_field, T value)
         {
             try
             {
-                string key = _MappingDic.Where(a => a.Value == db_field).FirstOrDefault().Key;
+                if (!_MappedDic.Any((a => a.Value == db_field)))
+                {
+                    throw new KeyNotFoundException($"映射的字典中并不包含名为{db_field}的Value");
+                }
+                string key = _MappedDic.Where(a => a.Value == db_field).FirstOrDefault().Key;
                 dr[key] = value;
             }
             catch (Exception ex)
             {
-                throw ex;
+                string key = _MappedDic.Where(a => a.Value == db_field).FirstOrDefault().Key;
+                throw new InRowChangingEventException($"名为{key}的列不支持赋予{typeof(T).Name}类型的值", ex.GetInnermostException());
             }
         }
-    
+
     }
 }
