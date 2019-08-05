e42f0760212c302aeed3dd18b56ac57a4bc05aaa
huangkemeng <huangkemeng@outlook.com>
2019-08-05 17:40:52 +0800

    添加了对DataTableHelper的单元测试

---
+++ KeMengUtils.Test/DataTableHelper-Test.cs
@@ -0,0 +1,33 @@
+﻿using KeMengUtils.DataHelper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using Xunit;
+
+namespace KeMengUtils.Test
+{
+    public class DataTableHelper_Test
+    {
+        public readonly DataTable dataTable;
+        public DataTableHelper_Test()
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("column");
+            DataRow dr = table.NewRow();
+            dr["column"] = "row";
+            table.Rows.Add(dr);
+            dataTable = table;
+        }
+
+        [Fact]
+        public void TryGetValueTest()
+        {
+            if (dataTable.Rows[0].TryGetValue("column", out object value))
+            {
+                var v = value;
+            }
+        }
+
+    }
+}
